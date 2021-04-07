8cd0eb7b6da985c6ab8e3070d90779ecef89aeda
Lucas-Buening <lbuening@purdue.edu>
2021-04-07 19:21:59 -0400

    Adjusted file structure. The catkin workspace is system specific, and will need to be setup on your individual machine to use ROS.

--- catkin_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/lucas/BRC_Biopayload/catkin_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/lucas/BRC_Biopayload/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/lucas/BRC_Biopayload/catkin_ws/devel;/opt/ros/noetic' != '' else []
